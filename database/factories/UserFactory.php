fec9ff5d10c7da91d4bfe8c180c9a8e709ddd8ea
Lewis MacCallum <w8h35@students.keele.ac.uk>
2019-05-04 20:15:37 +0100

    User Table Seeder & Default Admin
    Created User Table Seeder.
    Created Default Admin login credentials.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,10 +17,14 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'address' => $faker->address,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'bid' => str_pad(mt_rand(1,9999),4,'0',STR_PAD_LEFT),
+        'uid' => str_pad(mt_rand(1,9999),4,'0',STR_PAD_LEFT),
+        'fname' => $faker->firstName,
+        'lname' => $faker->lastName,
+        'number' => $faker->phoneNumber,
+        'email' => $faker->email,
     ];
 });
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UserTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
+
+        factory(App\User::class, 50)->create();
+
     }
 }
